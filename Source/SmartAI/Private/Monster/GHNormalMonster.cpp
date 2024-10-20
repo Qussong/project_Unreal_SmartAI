2dfa046635d5cc7a92c44025db541a22d841e753
Gihoon <89381041+Qussong@users.noreply.github.com>
2024-10-20 22:37:39 +0900

    Monster, Player Animation 정리

--- Source/SmartAI/Private/Monster/GHNormalMonster.cpp
+++ Source/SmartAI/Private/Monster/GHNormalMonster.cpp
@@ -1,23 +1,28 @@
 
 #include "Monster/GHNormalMonster.h"
+#include "Components/CapsuleComponent.h"
 
 AGHNormalMonster::AGHNormalMonster()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh>
-		MeshRef(TEXT(""));
+		MeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/_SmartAI/Monster/Zombie/Zombie.Zombie'"));
 	if (MeshRef.Succeeded())
 	{
 		GetMesh()->SetSkeletalMesh(MeshRef.Object);
 	}
-	
+	GetMesh()->SetRelativeLocation(FVector(0.f, -90.f, 0.f));
+	GetMesh()->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+
 	static ConstructorHelpers::FClassFinder<UAnimInstance>
-		AnimRef(TEXT(""));
+		AnimRef(TEXT("/Script/Engine.AnimBlueprint'/Game/_SmartAI/Monster/NormalMonster/ABP_Zombie.ABP_Zombie'"));
 	if (AnimRef.Succeeded())
 	{
 		GetMesh()->SetAnimClass(AnimRef.Class);
 	}
+	GetCapsuleComponent()->SetCapsuleSize(25.f, 90.f);
+
 
 }
 
---
+++ Source/SmartAI/Public/AIController/GHAIControllerBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "GHAIControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMARTAI_API AGHAIControllerBase : public AAIController
+{
+	GENERATED_BODY()
+	
+};
